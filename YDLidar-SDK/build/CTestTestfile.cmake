b9a6588d2f3eb51b79923eaca257bd6d80b38cb5
suzukiYU000 <syuma2001@keio.jp>
2025-06-30 13:25:42 +0900

    wip

---
+++ YDLidar-SDK/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ogawa/ogawa_ws/YDLidar-SDK
+# Build directory: /home/ogawa/ogawa_ws/YDLidar-SDK/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("src")
+subdirs("examples")
